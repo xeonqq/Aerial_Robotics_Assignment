b63b4a02201607587704930493520606f3155d83
xeonqq <xeonqq@gmail.com>
2016-12-15 17:53:57 +0100

    first commit

---
+++ assignment1_code/controller.m
@@ -0,0 +1,33 @@
+function [ u ] = pd_controller(~, s, s_des, params)
+%PD_CONTROLLER  PD controller for the height
+%
+%   s: 2x1 vector containing the current state [z; v_z]
+%   s_des: 2x1 vector containing desired state [z; v_z]
+%   params: robot parameters
+
+% FILL IN YOUR CODE HERE
+kp = 1;
+kd = 1;
+persistent vel_des_prev;
+
+if isempty(vel_des_prev)
+	vel_des_prev = 0;
+end
+
+u = 0;
+
+dt = 0.01;
+
+e = s_des - s;
+
+% e(1) error in z
+% e(2) error in v_z
+vel_des = s_des(2)
+
+ff = (vel_des - vel_des_prev)/dt;
+u = params.mass * (ff + kp*e(1) + kd*e(2) + params.gravity);
+
+vel_des_prev = vel_des;
+
+end
+
