6f31edf2baa858787f8d623f3f29e3188022f887
Qian <xeonqq@gmail.com>
2016-12-15 23:52:26 +0100

    tuned kp kd in assignment 1

--- assignment1_code/controller.m
+++ assignment1_code/controller.m
@@ -6,15 +6,14 @@ function [ u ] = pd_controller(~, s, s_des, params)
 %   params: robot parameters
 
 % FILL IN YOUR CODE HERE
-kp = 1;
-kd = 1;
+kp = 42;
+kd = 8;
 persistent vel_des_prev;
 
 if isempty(vel_des_prev)
 	vel_des_prev = 0;
 end
 
-u = 0;
 
 dt = 0.01;
 
@@ -22,9 +21,10 @@ e = s_des - s;
 
 % e(1) error in z
 % e(2) error in v_z
-vel_des = s_des(2)
+vel_des = s_des(2);
 
 ff = (vel_des - vel_des_prev)/dt;
+
 u = params.mass * (ff + kp*e(1) + kd*e(2) + params.gravity);
 
 vel_des_prev = vel_des;
