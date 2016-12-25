5fa16a5edc72da62108180b6a484293163dd0f51
Qian <xeonqq@gmail.com>
2016-12-25 21:40:28 +0100

    finished assignment 2

--- assignment2_code/controller.m
+++ assignment2_code/controller.m
@@ -14,10 +14,38 @@ function [ u1, u2 ] = controller(~, state, des_state, params)
 %   Using these current and desired states, you have to compute the desired
 %   controls
 
-u1 = 0;
-u2 = 0;
-
 % FILL IN YOUR CODE HERE
+err_pos = des_state.pos - state.pos;
+err_vel = des_state.vel - state.vel;
+
+kvz = 10;
+kpz = 42;
+
+kvy = 10;
+kpy = 25;
+
+kvphi = 6.27*1/ 8 * 40;
+kpphi = 1*0.8 * 500;
+
+phi_ddot_des = 0;
+phi_dot_des = 0;
+
+
+u1 = params.mass * (params.gravity + des_state.acc(2) + err_pos(2) * kpz + err_vel(2)*kvz);
+
+%dt = 0.01;
+
+phi_des = -(des_state.acc(1) + kpy * err_pos(1) + err_vel(1)*kvy)/params.gravity;
+
+%phi_des = -pi/6;
+err_phi = phi_des - state.rot;
+err_phi_dot = phi_dot_des - state.omega;
+
+
+u2 = params.Ixx * (phi_ddot_des + kpphi* err_phi + kvphi*err_phi_dot);
+
+
+
 
 end
 
--- assignment2_code/runsim.m
+++ assignment2_code/runsim.m
@@ -7,7 +7,9 @@ addpath('trajectories');
 controlhandle = @controller;
 
 % Choose which trajectory you want to test with
-trajhandle = @traj_line;
-% trajhandle = @traj_sine;
+%trajhandle = @traj_line;
+%trajhandle = @traj_sine;
+%trajhandle = @traj_step;
+trajhandle = @traj_diamond;
 
 [t, state] = simulation_2d(controlhandle, trajhandle);
